f66df4749a77d71f471a9faee182dfe27f8b6d3c
Sebastien Dolce <32372446+SebastienDolce@users.noreply.github.com>
2026-08-26 12:34:37 -0400

    Lock proposal class to server-only access

--- apps/OpenSignServer/databases/migrations/20260826163000-create-proposal-class.cjs
+++ apps/OpenSignServer/databases/migrations/20260826163000-create-proposal-class.cjs
@@ -1,32 +1,45 @@
 /**
  * Store immutable sent/accepted proposal snapshots.
+ * The class is master-key only; clients use the authenticated/public proposal routes.
  *
  * @param {Parse} Parse
  */
 exports.up = async Parse => {
   const schema = new Parse.Schema('contracts_Proposal');
-  schema.addString('ProposalNumber');
-  schema.addString('Name');
-  schema.addString('Status');
-  schema.addString('RecipientName');
-  schema.addString('RecipientEmail');
-  schema.addString('HtmlContent');
-  schema.addString('DarkCss');
-  schema.addString('LightCss');
-  schema.addString('DarkPdfUrl');
-  schema.addString('LightPdfUrl');
-  schema.addString('SnapshotHash');
-  schema.addString('PublicTokenHash');
-  schema.addString('HtmlTemplateId');
-  schema.addString('ContractTemplateId');
-  schema.addString('ContractDocumentId');
-  schema.addString('ContactBookId');
-  schema.addString('AcceptedIp');
-  schema.addDate('SentAt');
-  schema.addDate('AcceptedAt');
-  schema.addPointer('CreatedBy', '_User');
-  schema.addPointer('ExtUserPtr', 'contracts_Users');
-  return schema.save();
+  schema
+    .addString('ProposalNumber')
+    .addString('Name')
+    .addString('Status')
+    .addString('RecipientName')
+    .addString('RecipientEmail')
+    .addString('HtmlContent')
+    .addString('DarkCss')
+    .addString('LightCss')
+    .addString('DarkPdfUrl')
+    .addString('LightPdfUrl')
+    .addString('SnapshotHash')
+    .addString('PublicTokenHash')
+    .addString('HtmlTemplateId')
+    .addString('ContractTemplateId')
+    .addString('ContractDocumentId')
+    .addString('ContactBookId')
+    .addString('AcceptedIp')
+    .addDate('SentAt')
+    .addDate('AcceptedAt')
+    .addPointer('CreatedBy', '_User')
+    .addPointer('ExtUserPtr', 'contracts_Users')
+    .addIndex('proposal_public_token_hash_1', { PublicTokenHash: 1 })
+    .addIndex('proposal_number_1', { ProposalNumber: 1 })
+    .setCLP({
+      get: {},
+      find: {},
+      count: {},
+      create: {},
+      update: {},
+      delete: {},
+      addField: {},
+    });
+  return schema.save(null, { useMasterKey: true });
 };
 
 /**
